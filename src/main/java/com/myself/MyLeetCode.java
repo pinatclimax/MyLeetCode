0f255178f8596d1bce204751627492ceb6ec56fa
Pin <pin@climax.com.tw>
2021-04-02 09:50:03 +0800

    upload(easy/array): rotate array

--- src/main/java/com/myself/MyLeetCode.java
+++ src/main/java/com/myself/MyLeetCode.java
@@ -2,44 +2,59 @@ package com.myself;
 
 public class MyLeetCode {
     public static void main(String[] args) {
-        int[] arr = {1, 1, 2, 2, 3, 4, 4, 5, 6};
-        int length = new Solution().removeDuplicates(arr);
-        System.out.println("length: " + length);
-
-        for (int x = 0; x < length; x++) {
-            System.out.print(arr[x] + " ");
-        }
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        new Solution().rotate(arr, 3);
     }
 }
 
 class Solution {
-    public int removeDuplicates(int[] nums) {
-
-        if (nums.length == 1) {
-            return 1;
+    public void rotate(int[] nums, int k) {
+        if (k <= 0 || k == nums.length) {
+            return;
         }
 
-        int curIdx = 0;
-        for (int i = 0; i < nums.length; i++) {
-
-            if (i + 1 == nums.length) {
-                nums[curIdx] = nums[nums.length - 1];
-                continue;
+        if (k > nums.length) {
+            k = k % nums.length;
+        }
+        System.out.println("k=" + k);
+        int[] newNums = nums.clone();
+        for (int i = 0; i < newNums.length; i++) {
+            if ((k + i) >= nums.length) {
+                nums[(k + i) % nums.length] = newNums[i];
+            } else {
+                nums[k + i] = newNums[i];
             }
-
-            if (!(nums[i + 1] == nums[i])) {
-                nums[curIdx] = nums[i];
-                curIdx += 1;
-            }
-
-
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i] + ", ");
+        for (int x = 0; x < nums.length; x++) {
+            System.out.print(nums[x] + " ");
         }
         System.out.println();
-
-        return curIdx + 1;
+        for (int x = 0; x < nums.length; x++) {
+            System.out.print(newNums[x] + " ");
+        }
     }
+
+    // v1
+//    public void rotate(int[] nums, int k) {
+//        if (k <= 0) {
+//            return;
+//        }
+//
+//        int tmp = 0;
+//        for (int j = 0; j < k; j++) {
+//            for (int i = nums.length - 1; i >= 0; i--) {
+//                if (i == nums.length - 1) {
+//                    tmp = nums[i];
+//                    continue;
+//                }
+//                nums[i + 1] = nums[i];
+//            }
+//            nums[0] = tmp;
+//        }
+//
+//        for (int x = 0; x < nums.length; x++) {
+//            System.out.print(nums[x] + " ");
+//        }
+//    }
 }
